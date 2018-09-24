e4c1c237bf6dc9bebcec1942571b50d89986a9aa
Wali Rahman <wali.rahman89@yahoo.com>
2018-09-24 00:46:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,9 @@ function BoardMember (name, homeState, training){
   this.homeState = homeState
   this.name = name
   this.training = training
+  this.veto = function(){
+    console.log("No, I must disagree");
+    
+  }
   
 }
